6d19e23bb5a76db80841face0094a8eb0748a368
Theresa Schmotz <Theresa@zuhause.local>
2018-11-21 23:31:34 +0100

    Aufgabe 2 UE1

---
+++ src/Aufgab2_1810653557.java
@@ -0,0 +1,20 @@
+public class Aufgab2_1810653557 {
+
+    public static void main(String[]args)
+    {
+        String name = "Sheldon Cooper";         //Wertebereich Zeichenanreihungen
+        int age = 28;                           //Wertebereich -2,147,483,648 bis 2,147,483,647 Ganzzahl
+        double exactAge = 28.678;               //Wertebereich ca. 4.9 x 10^-324 bis 1.7 x 10^308 => Fießkommazahl
+        boolean sheldonHatRecht = true;         //Wertebereich true/false => Warheitswert
+        long a = 10;                            //Wertebereich -9,223,372,036,854,775,808 bis 9,223,372,036,854,775,807 => Ganzzahl
+        char zeichen = 'K';                     //Wertebereich 65,536 unterschiedliche Zeichen => Zeichen
+        byte b = -120;                          //Wertebereich -128 bis 127 => Ganzzahl
+        short c = 50;                           //Wertebereich -32,768 bis 32,767 => Ganzzahl
+        float d = 4;                            //Wertebereich ca. 1.4 x 10^-45 bis 3.4 x 10^38 => Fließkommazahl
+
+        //Konstante
+        final int v = 218;
+        //Final, also die Konstante, legt einen unveränderbaren Grenzwert fest. Variablen jedoch können nachträglich noch geändert werden.
+
+    }
+}
